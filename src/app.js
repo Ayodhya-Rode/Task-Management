f41defbbf41d4ce2e3747eab84667b622d41c628
Ayodhya Rode <ayodhyarode168@gmail.com>
2026-07-14 15:56:18 +0530

    Task related routes imported

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import express from "express"
 import morgan from 'morgan';
 import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.routes.js"
-
+import taskRoutes from "./routes/task.routes.js"
 
 const app = express()
 app.use(cookieParser());
@@ -13,6 +13,7 @@ app.use(morgan('dev'));
 
 
 app.use("/api/auth", authRoutes)
+app.use("/api/tasks", taskRoutes)
 
 
 app.get("/", (req, res)=>{
