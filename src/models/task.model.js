46d27328a0b5481bb443ef57a430890ea144d721
Ayodhya Rode <ayodhyarode168@gmail.com>
2026-07-14 15:56:41 +0530

    Task MOdel created

---
+++ src/models/task.model.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+const taskSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+      trim: true,
+    },
+    status: {
+      type: String,
+      enum: ["pending", "in-progress", "completed"],
+      default: "pending",
+    },
+    priority: {
+      type: String,
+      enum: ["low", "medium", "high"],
+      default: "medium",
+    },
+    dueDate: {
+      type: Date
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },{ timestamps: true },
+);
+
+const taskModel = mongoose.model("Task", taskSchema);
+export default taskModel;
